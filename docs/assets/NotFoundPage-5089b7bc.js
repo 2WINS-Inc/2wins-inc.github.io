108450e1d5cc076b8f95b06b3f8b786571c57120
Kai Wang <kaiwdev@gmail.com>
2023-09-15 19:54:57 +0900

    [update] Update build

---
+++ docs/assets/NotFoundPage-5089b7bc.js
@@ -0,0 +1,5 @@
+import{i,c as r,A as s,t as e}from"./index-d288b46f.js";const m=e('<div class="fixed left-0 top-0 -z-20"><video src="/media/hero.mp4" muted autoplay playsinline loop class="min-w-screen min-h-screen object-cover opacity-40">'),a=e('<div class="flex min-h-screen flex-col items-center justify-center"><p class="my-6 text-4xl font-semibold tracking-wide md:my-10 md:text-7xl lg:text-8xl">404 Not Found</p><p class="my-3 text-xl font-medium tracking-wider md:my-8 md:text-3xl lg:text-4xl">ご指定のページが見つかりません</p><div class="mt-8">'),d=()=>[m(),(()=>{const t=a(),n=t.firstChild,o=n.nextSibling,l=o.nextSibling;return i(l,r(s,{href:"/",class:`
+        block h-fit w-fit border-2
+        border-onBackground bg-gradient-to-l from-background from-50% to-onBackground to-50% bg-[size:200%] bg-right px-8 
+        py-4 text-sm font-medium tracking-[0.25rem] transition-all duration-500 ease-in-out hover:bg-left hover:text-background md:px-16 md:text-lg
+      `,children:"ホームに戻る"})),t})()];export{d as default};
